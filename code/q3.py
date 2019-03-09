421413bbfda6be0c74aa0927e2abb4dfe43becf8
Abraham Torres <jabraham@gmail.com>
2019-03-08 20:42:34 -0800

    Q4 done

--- code/q3.py
+++ code/q3.py
@@ -2,11 +2,13 @@ import numpy as np
 import sys
 
 def M(n, Soln):
-    if n <= 1:
-        return 1
-    if Soln[n-1] != 1:
-        return Soln[n-1]
     Min = sys.maxsize
+    if n == 1:
+        return 1
+
+    if Soln[n-1] != -1:
+        return Soln[n-1]
+
     for i in range(1,n):
         Min = min(Min, M(n-i, Soln) + M(i,Soln))
         if (i+1 < n/2 and n%(i+1) == 0):
@@ -16,5 +18,5 @@ def M(n, Soln):
     return Min
 
 n = 14
-Soln = np.full(n,1)
+Soln = np.full(n,-1)
 print(M(n,Soln))
